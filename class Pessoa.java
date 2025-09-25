826a4b5aad732a56b47214be9664e90c3e82cf34
Enzo <guedesazk73@gmail.com>
2025-09-24 22:33:25 -0300

    Classe Pessoa

---
+++ class Pessoa.java
@@ -0,0 +1,18 @@
+public class Pessoa {
+    protected String nome;
+    protected int idade;
+    protected String cpf;
+
+    public Pessoa(String nome, int idade, String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+
+    public void mostrarDados() {
+        System.out.println("=== Dados da Pessoa ===");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("CPF: " + cpf);
+    }
+}
